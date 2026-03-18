a2ab042ab2d6ae21beb871c7246070e401a6c7f6
Satya <satya@Sachs-MacBook-Air.local>
2026-03-19 00:01:02 +0530

    Initial commit: FastAPI CRUD setup with MySQL

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ from fastapi import FastAPI, Path, Request
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return " Home page "
+
+@app.get("/products")
+def get_all_products():
+    return "all products"
